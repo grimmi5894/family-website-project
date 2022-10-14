53ff3d4822722c279aec785609b17d0c4df95321
grimmi5894 <79322778+grimmi5894@users.noreply.github.com>
2022-10-14 12:39:17 -0400

    downloaded initial dependencies, setup express app, initial scripts, eslint

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (request, response) => {
+  return response.send('Testing')
+})
+
+app.all('*', (request, response) => {
+  return response.sendStatus(404)
+})
+
+
+app.listen(1337, () => {
+  console.log('Listening on 1337') // eslint-disable-line no-console
+})
